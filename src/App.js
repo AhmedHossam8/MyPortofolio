5a62ce3d61501eae15c77fffb6f40ac2db1b8a0f
Shaimaa Hossam Emara <ahmedhossamemara8@gmail.com>
2023-08-31 20:17:07 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Homepage from './components/Homepage.js';
+import Navbar from './components/Navbar.js';
+
+const App = () => {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Homepage></Homepage>
+    </div>
+  );
+}
+
+export default App;
