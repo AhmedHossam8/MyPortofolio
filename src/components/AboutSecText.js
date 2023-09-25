b702c8e94ae1680aca3accce9e9aa040faf8d21b
Ahmed Emara <ahmedhossamemara8@gmail.com>
2023-09-25 14:24:06 +0300

    moved the about text from the js file to json file

--- src/components/AboutSecText.js
+++ src/components/AboutSecText.js
@@ -1,25 +1,26 @@
 import React from 'react';
+import aboutSecData from '../data/aboutSecData.json';
 
 function AboutSecText() {
-  const listItemStyle = { color: 'white' }; // Style for list items
+  const listItemStyle = { color: 'white' };
 
   return (
     <div>
-      <p>
-        <strong style={{color: "aquamarine"}}>Internship Experience:</strong>
-        <ul>
-          <li style={listItemStyle}>My 2023 ITWorx internship transformed me, enhancing technical skills and creative problem-solving. A highlight was co-developing an AI-driven comic app that merges user prompts with AI images for captivating comic strips. This spanned AI APIs, image processing, and frontend work. Collaborating with experts, I grasped iterative tech refinement and learned adaptability.</li>
-          <li style={listItemStyle}>Resulting from my ITWorx internship, the AI comic app epitomizes tech-creativity harmony. It interprets user prompts, AI-curates images, and creates dynamic comics. I contributed in AI integration, image processing, and frontend. This deepened my tech prowess and renewed my innovation dedication. Excited to further leverage tech for impactful strides in computer science.</li>
-        </ul>
-      </p>
-      <p>
-        <strong style={{color: "aquamarine"}}>Future Aspirations:</strong>
-        <ul>
-          <li style={listItemStyle}>My journey in the world of Computer Science is an ever-evolving adventure.</li>
-          <li style={listItemStyle}>Eagerly leveraging my skills and experiences to push boundaries and create a meaningful impact.</li>
-          <li style={listItemStyle}>Continuing to explore new horizons, collaborate on innovative projects, and strive for excellence in the ever-dynamic realm of technology.</li>
-        </ul>
-      </p>
+      {aboutSecData.sections.map((section, index) => (
+        <div key={index}>
+          <p>
+            <strong style={{ color: 'aquamarine' }}>{section.title}</strong>
+            <ul>
+              {section.content.map((item, itemIndex) => (
+                <li key={itemIndex} style={listItemStyle}>
+                  {item}
+                </li>
+              ))}
+            </ul>
+          </p>
+          <br />
+        </div>
+      ))}
     </div>
   );
 }
--- src/components/Abouttext.js
+++ src/components/Abouttext.js
@@ -1,33 +1,26 @@
 import React from 'react';
+import aboutData from '../data/aboutData.json';
 
 function Abouttext() {
-  const listItemStyle = { color: 'white' }; // Style for list items
+  const listItemStyle = { color: 'white' };
 
   return (
     <div>
-      <p>
-        <strong style={{color: "aquamarine"}}>Academic Excellence:</strong>
-        <ul>
-          <li style={listItemStyle}>I'm Ahmed Emara, an enthusiastic undergraduate student majoring in Computer Science.</li>
-          <li style={listItemStyle}>I've consistently secured the top position in my class for two consecutive semesters; a testament to my insatiable curiosity and unwavering dedication to excellence.</li>
-        </ul>
-      </p>
-      <br />
-      <p>
-        <strong style={{color: "aquamarine"}}>Competitive Achievement:</strong>
-        <ul>
-          <li style={listItemStyle}>Representing my university at the ECPC in summer 2022.</li>
-          <li style={listItemStyle}>Collaborated with exceptional peers to conquer intricate coding challenges, showcasing my resilience in real-world problem-solving scenarios.</li>
-        </ul>
-      </p>
-      <br />
-      <p>
-        <strong style={{color: "aquamarine"}}>Teaching and Mentorship:</strong>
-        <ul>
-          <li style={listItemStyle}>Beyond the classroom, I've embraced a role as a problem-solving instructor.</li>
-          <li style={listItemStyle}>Guiding fellow students through coding challenges, deepening my understanding and igniting a passion for teaching and mentorship.</li>
-        </ul>
-      </p>
+      {aboutData.sections.map((section, index) => (
+        <div key={index}>
+          <p>
+            <strong style={{ color: 'aquamarine' }}>{section.title}</strong>
+            <ul>
+              {section.content.map((item, itemIndex) => (
+                <li key={itemIndex} style={listItemStyle}>
+                  {item}
+                </li>
+              ))}
+            </ul>
+          </p>
+          <br />
+        </div>
+      ))}
     </div>
   );
 }
